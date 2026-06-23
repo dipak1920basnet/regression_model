243bd29d7a5f2ce03c0258ba0517212606104b6f
dipak1920basnet <dipak1920basnet@gmail.com>
2026-06-23 08:52:05 +0545

    implement gradient functions

--- gradient_descent.py
+++ gradient_descent.py
@@ -5,11 +5,17 @@
 
 """
 
+import sumpy as sp 
+
+
 # Calculate Gradient 
 
-def Gradient(parameter, function):
+def Gradient(parameter_value, parameter:str, function:str):
     # get the gradient of function with respect to the parameter
-    pass
+    x = sp.Symbol(parameter)
+    df = sp.diff(function, x)
+    value = df.subs(x, parameter_value)
+    return value
 
 
 # Update the parameter
@@ -24,8 +30,18 @@ def Gradient_descent(w,b,alpha,function):
     alpha: learning rate
     function: function to minimize eg: cost
     """
-    weight_Gradient, bias_gradient = Gradient(w, function), Gradient(b, function)
+    weight_Gradient, bias_gradient = Gradient("w", function), Gradient("b", function)
     # return updated weight and bias
     return update(w,alpha,weight_Gradient), update(b,alpha, bias_gradient)
 
-    
+
+"""
+def train_model(w,b,alpha,cost_function,epochs):
+    w_history = [-1]*epochs
+    b_history = [-1]*epochs
+    w_history.append(w)
+    b_history.append(b)
+    for i in epochs:
+        w,b = Gradient_descent(w,b,alpha,cost_function)
+    return w_history,b_history
+"""
